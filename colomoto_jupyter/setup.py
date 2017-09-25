5e7d8ed0da3dfbd1ab68f41aae4a1342d2de07a0
Loïc Paulevé <loic.pauleve@ens-cachan.org>
2017-09-25 16:10:38 +0200

    switch to networkx 2.0 API

--- colomoto_jupyter/setup.py
+++ colomoto_jupyter/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 NAME = "colomoto_jupyter"
 
 setup(name=NAME,
-    version='0.3.2',
+    version='0.3.3',
     author = "Loïc Paulevé",
     author_email = "loic.pauleve@ens-cachan.org",
     url = "https://github.com/colomoto/colomoto-api",
@@ -15,7 +15,7 @@ Provides helper functions for integration in the CoLoMoTo Jupyter notebook.
 See https://github.com/colomoto/colomoto-api
 """,
     extras_require = {
-        "networkx": ["networkx", "pydotplus", "pygraphviz"],
+        "networkx": ["networkx >= 2.0", "pydot", "pygraphviz"],
     },
     license="LGPL v3+/CeCILL-C",
     include_package_data = True,
