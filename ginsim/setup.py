f3d7456afdca41d79e23c91345f9e270adce49dc
Loïc Paulevé <loic.pauleve@ens-cachan.org>
2017-10-18 14:10:28 +0200

    ginsim setup.py

--- ginsim/setup.py
+++ ginsim/setup.py
@@ -1,9 +1,24 @@
-from setuptools import setup
+
+from setuptools import setup, find_packages
 
 NAME = 'ginsim'
-setup(
-    name=NAME,
+
+setup(name=NAME,
     version='0.1',
-    py_modules = [NAME]
+    author = "Aurélien Naldi",
+    author_email = "aurelien.naldi@gmail.com",
+    url = "https://github.com/colomoto/colomoto-api",
+    description = "Python interface to GINsim and BioLQM API",
+    long_description = """
+Provides interface to Java API of GINsim and BioLQM
+""",
+    install_requires = "py4j",
+    license="LGPL v3+/CeCILL-C",
+    packages = find_packages(),
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+    ],
+    keywords="computational systems biology",
 )
 
