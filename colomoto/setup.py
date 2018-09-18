cc217f43e695b6717509f4549e89c58ff123add8
Loïc Paulevé <loic.pauleve@ens-cachan.org>
2018-09-18 15:29:45 +0200

    complete colomoto.setup_helper to install pkg outside conda

---
+++ colomoto/setup.py
@@ -0,0 +1,3 @@
+from colomoto.setup_helper import setup
+setup({"pkg": "colomoto/nusmv",
+        "check_progs": ["NuSMV"]})
--- colomoto/setup_helper.py
+++ colomoto/setup_helper.py
@@ -2,7 +2,8 @@
 import json
 import platform
 import os
-import stat
+import shutil
+import sys
 import tarfile
 
 try:
@@ -10,13 +11,23 @@ try:
 except:
     # Python 2
     from urllib import urlretrieve
-    from urlib2 import urlopen
+    from urllib2 import urlopen
+
+if platform.system() == "Windows":
+    PREFIXES = [os.getenv("APPDATA"), "colomoto"]
+else:
+    PREFIXES = ["/usr/local/share/colomoto",
+        os.path.join(os.path.expanduser("~"), ".local", "share", "colomoto")]
+
+paths = ":".join([p for p in PREFIXES if os.path.exists(p)])
+os.environ["PATH"] = "%s:%s" % (paths, os.environ["PATH"])
 
 def conda_package_url(name, version=None, label="main"):
     system = platform.system().lower()
     machine = platform.machine()
-    with urlopen("http://api.anaconda.org/package/{}".format(name)) as fd:
-        data = json.load(fd)
+    fd = urlopen("http://api.anaconda.org/package/{}".format(name))
+    data = json.load(fd)
+    fd.close()
     if version is None:
         version = data["latest_version"]
     b = None
@@ -33,4 +44,69 @@ def conda_package_url(name, version=None, label="main"):
         if b is None or f["attrs"]["build_number"] > b["attrs"]["build_number"]:
             b = f
     return "http:{}".format(b["download_url"]) if b else None
+
+def prepare_dest(dest):
+    destdir = os.path.dirname(dest)
+    if not os.path.exists(destdir):
+        os.makedirs(destdir)
+
+def conda_package_extract(conda_url, prefix, subdirs=['bin','opt']):
+    print("downloading {}".format(conda_url))
+    localfile = urlretrieve(conda_url)[0]
+    fmt = conda_url.split(".")[-1]
+    def match_member(m):
+        return m.name.split('/')[0] in subdirs
+    with tarfile.open(localfile, "r:%s"%fmt) as tar:
+        for m in tar:
+            if m.isreg() and match_member(m):
+                dest = os.path.join(prefix, m.name)
+                fd = tar.extractfile(m)
+                print("installing %s" % dest)
+                prepare_dest(dest)
+                with open(dest, "wb") as o:
+                    o.write(fd.read())
+                fd.close()
+                os.chmod(dest, m.mode)
+    os.unlink(localfile)
+
+def is_installed(progname):
+    if sys.version_info[0] < 3:
+        paths = os.environ["PATH"].split(":")
+        for p in paths:
+            if os.path.exists(os.path.join(p, progname)):
+                return True
+        return False
+    return shutil.which(progname) is not None
+
+def installation_prefix():
+    if platform.system() == "Windows":
+        return PREFIXES[0]
+    if os.getuid() == 0:
+        return PREFIXES[0]
+    else:
+        return PREFIXES[-1]
+
+def setup(*specs):
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+    parser.add_argument("-f", "--force", default=False, action="store_true",
+            help="Force installation")
+    args = parser.parse_args()
+    prefix = installation_prefix()
+    for spec in specs:
+        if not args.force:
+            print("# checking for {}".format(spec["pkg"]))
+            skip = True
+            for prog in spec.get("check_progs", []):
+                if not is_installed(prog):
+                    skip = False
+                    break
+            if skip and "check_install" in spec:
+                skip = spec["check_install"]()
+            if skip:
+                print("# {} is already installed.".format(spec["pkg"]))
+                continue
+        print("# installing {} in {}".format(spec["pkg"], prefix))
+        pkg = conda_package_url(spec["pkg"])
+        conda_package_extract(pkg, prefix)
 
