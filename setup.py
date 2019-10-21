91f1c0b9a65a2be7237974b17a55d24c4841d090
Loïc Paulevé <loic.pauleve@ens-cachan.org>
2019-10-21 11:51:32 +0200

    v0.5.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 NAME = "colomoto_jupyter"
 
 setup(name=NAME,
-    version='0.5.7',
+    version='0.5.8',
     author = "Loïc Paulevé",
     author_email = "loic.pauleve@ens-cachan.org",
     url = "https://github.com/colomoto/colomoto-jupyter",
