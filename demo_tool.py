a5c403d2e7697b6cc3e20b1dc6fd4d2d2eb61eac
Gerald Point <gerald.point@labri.fr>
2025-05-21 12:50:39 +0200

    update jupyter_setup with ipylab API

--- demo_tool.py
+++ demo_tool.py
@@ -1,64 +1,102 @@
+from time import sleep
 
 from colomoto_jupyter import IN_IPYTHON, jupyter_setup
+from colomoto_jupyter.ipylab import (
+    ipylab_insert_snippet,
+    ipylab_insert_run_snippet,
+    ipylab_upload_and_process_filename,
+    ipylab_upload_and_process_filenames,
+)
 
 if IN_IPYTHON:
     from IPython.display import display, FileLink
 
     menu = [
-        {"name": "Load model",
-            "snippet": "model = demo_tool.load(\"filename\")"},
-        {"name":"Upload model",
-            "snippet": "model = demo_tool.upload()"}, # use lists for multiple lines
+        {"name": "Load model", "snippet": 'model = demo_tool.load("filename")'},
+        {
+            "name": "Upload models",
+            "snippet": "demo_tool.upload_many('model')",
+        },  # use lists for multiple lines
         "---",
-        {"name":"Model export",
+        {
+            "name": "Model export",
             "sub-menu": [
-            {"name": "Format 1 (.f1)",
-                "snippet": 'model.export("model.f1")'},
-            {"name": "Format 2 (.f2)",
-                "snippet": 'model.export("model.f2")'},
-            ]},
+                {"name": "Format 1 (.f1)", "snippet": 'model.export("model.f1")'},
+                {"name": "Format 2 (.f2)", "snippet": 'model.export("model.f2")'},
+            ],
+        },
+        {
+            "name": "About",
+            "sub-menu": [
+                {"name": "CoLoMoTo", "external-link": "https://colomoto.github.io/"},
+                {
+                    "name": "CoLoMoTo Jupyter",
+                    "external-link": "https://github.com/colomoto/colomoto-jupyter",
+                },
+            ],
+        },
     ]
 
     toolbar = [
-        {"name": "upload",
-         "setup": {
-            "icon": "fa-upload",
-            "help": "Upload model",
-            "handler": "action_upload_model"}},
+        {
+            "name": "upload",
+            "setup": {
+                "icon": "fa fa-upload",
+                "label": __name__,
+                "help": "Upload model",
+                "handler": ipylab_insert_snippet,
+                "args": {"snippet": f"{__name__}.upload('model')"},
+            },
+        },
     ]
-    js_api = {
-        "action_upload_model":
-            """function () {
-            var cell = Jupyter.notebook.get_selected_cell();
-            cell.set_text('model = '+demo_tool_jsapi.module_alias+'.upload()');
-            cell.focus_editor(); }
-            """
-    }
-    jupyter_setup(__name__,
+
+    js_api = {}
+
+    jupyter_setup(
+        __name__,
         label="Test",
-        color="blue",
+        color="white",
+        bgcolor="lightblue",
         menu=menu,
         toolbar=toolbar,
-        js_api=js_api
+        js_api=js_api,
     )
+
 
 class DummyModel:
     def __init__(self, filename):
         self.filename = filename
+
     def __repr__(self):
         return "<DummyModel: %s>" % self.filename
+
     def export(self, output):
         with open(output, "w") as out:
             out.write("TEST")
         if IN_IPYTHON:
             display(FileLink(output))
 
+
 def load(filename):
     return DummyModel(filename)
 
+
 if IN_IPYTHON:
-    from colomoto_jupyter.upload import jupyter_upload
-    def upload():
-        return jupyter_upload(upload, load)
 
+    def upload(model: str):
+        def callback(filename: str):
+            ipylab_insert_run_snippet(f"{model} = {__name__}.load('{filename}')")
 
+        ipylab_upload_and_process_filename(callback)
+
+    def upload_many(model: str):
+        def callback(filenames: list[str]):
+            snippet = []
+            i = 0
+            for filename in filenames:
+                snippet.append(f"{model}_{i} = {__name__}.load('{filename}')")
+                i += 1
+            snippet.append(", ".join([f"{model}_{i}" for i in range(0,i)]))
+            ipylab_insert_run_snippet(snippet)
+
+        ipylab_upload_and_process_filenames(callback)
